13abc0a81edbc50a1ae092d85771d91c3e44295e
NishatFariza <nishatfarzia78@gmail.com>
2022-10-06 00:20:55 +0600

    implement scss

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hero: React.FC = () => {
   return (
-    <div className='bg-red-700 h-56 hero-section'>
+    <div className=' h-56 hero-section'>
         <h1>Navbar</h1>
     </div>
   )
