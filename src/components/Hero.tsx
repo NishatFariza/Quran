37f9f41db0eeb2e4946a209da9bde42b0d7edc67
NishatFariza <nishatfarzia78@gmail.com>
2022-10-06 00:16:28 +0600

    install sass

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Hero: React.FC = () => {
   return (
-    <div className='bg-red-700 h-56'></div>
+    <div className='bg-red-700 h-56 hero-section'>
+        <h1>Navbar</h1>
+    </div>
   )
 }
 
