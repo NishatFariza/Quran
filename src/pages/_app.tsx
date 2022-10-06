ef9c8aecb5bd7825af117b00fdd3e64027164dc2
NishatFariza <nishatfarzia78@gmail.com>
2022-10-06 07:48:22 +0600

    layout component added with interface

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,17 @@
 import '../styles/globals.scss'
 import 'antd/dist/antd.css';
 import type { AppProps } from 'next/app'
+import Navbar from '../components/Navbar';
+import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+       
+  </>
 }
 
 export default MyApp
