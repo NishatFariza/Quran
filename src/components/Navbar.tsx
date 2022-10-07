d5d883c5d3938edb50e44dfde1a82420d9fe37df
NishatFariza <nishatfarzia78@gmail.com>
2022-10-07 23:35:02 +0600

    navbar done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,21 +2,21 @@ import React from 'react'
 
 const Navbar: React.FC = () => {
   return (
-    <div className='flex justify-between px-8 py-5'>
-        <h1 className=''>Quran</h1>
+    <div className='flex justify-between px-8 py-5 fixed top-0 left-0 w-full'>
+        <h1 className='text-xl'>Quran</h1>
         <nav className='flex items-center gap-4'>
-            <a href="#">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+            <button>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 text-white">
   <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
 </svg>
 
-            </a>
-            <a href="#">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            </button>
+            <button>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 text-white">
   <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
 </svg>
 <s></s>
-            </a>
+            </button>
         </nav>
     </div>
   )
