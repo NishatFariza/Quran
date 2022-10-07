f4a2ee54665145a9cc77d40c6349e8a798f85cdf
NishatFariza <nishatfarzia78@gmail.com>
2022-10-08 04:42:24 +0600

    quran api call

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,16 @@
-import '../styles/globals.scss'
-import 'antd/dist/antd.css';
-import type { AppProps } from 'next/app'
-import Navbar from '../components/Navbar';
-import Layout from '../components/Layout';
+import "../styles/globals.scss";
+import "antd/dist/antd.css";
+import type { AppProps } from "next/app";
+import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <>
-
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
-       
-  </>
+  return (
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,36 @@
-import Head from 'next/head'
-import React from 'react'
-import Hero from '../components/Hero'
-import { Button } from 'antd';
-import Surah from '../components/Surah';
+import Head from "next/head";
+import React, { useEffect, useState } from "react";
+import Hero from "../components/Hero";
+import Surah from "../components/Surah";
+import axios from "axios";
 
 const HomePage = () => {
+  const [surahs, setSurahs] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://api.quran.com/api/v4/chapters?language=en")
+      .then((res) => {
+        console.log(res.data);
+      });
+  });
+
   return (
     <div>
       <Head>
         <title>Homepage</title>
       </Head>
 
-      <Hero/>
-     <div className='wrapper py-20'>
-       <h2 className='uppercase font-semibold text-xl'>Surah</h2>
+      <Hero />
+      <div className="wrapper py-20">
+        <h2 className="uppercase font-semibold text-xl">Surah</h2>
 
-        <div className='grid grid-cols-1 md:grid-cols-3 gap-5 sm:grid-cols-2'>
-          <Surah name='Al-Fatiha' meaning='The Opener' serial={1}/>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-5 sm:grid-cols-2">
+          <Surah name="Al-Fatiha" meaning="The Opener" serial={1} />
         </div>
-     </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
