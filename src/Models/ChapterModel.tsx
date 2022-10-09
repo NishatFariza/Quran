57f230a3949171c1229bb5f6ebbdad4c9919c9e8
NishatFariza <nishatfarzia78@gmail.com>
2022-10-09 09:31:43 +0600

    add react query

---
+++ src/Models/ChapterModel.tsx
@@ -0,0 +1,26 @@
+export enum RevelationPlace {
+  Madinah = "madinah",
+  Makkah = "makkah",
+}
+
+export interface TranslatedName {
+  language_name: LanguageName;
+  name: string;
+}
+
+export enum LanguageName {
+  English = "english",
+}
+
+export interface Chapter {
+  id: number;
+  revelation_place: RevelationPlace;
+  revelation_order: number;
+  bismillah_pre: boolean;
+  name_simple: string;
+  name_complex: string;
+  name_arabic: string;
+  verses_count: number;
+  pages: number[];
+  translated_name: TranslatedName;
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,18 @@ import "../styles/globals.scss";
 import "antd/dist/antd.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+// Create a client
+const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </>
+    </QueryClientProvider>
   );
 }
 
