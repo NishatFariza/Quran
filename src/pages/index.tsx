68539869c13e09168c79b2c5434064268d3efce3
NishatFariza <nishatfarzia78@gmail.com>
2022-10-15 23:52:12 +0600

    create ssr interface

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,14 +5,21 @@ import Surah from "../components/Surah";
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
 import { Chapter } from "../Models/ChapterModel";
+import { GetServerSideProps, NextPage } from "next";
 
-const HomePage = () => {
+interface props{
+   x: string
+ }
+
+
+
+const HomePage: NextPage<props> = ({x}) => {
   const [chapters, setChapters] = useState([]);
 
-  const { data } = useQuery(["chapters"], async () => {
-    const res = await axios.get("https://api.quran.com/api/v3/chapters");
-    return res.data.chapters as Chapter[];
-  });
+  // const { data } = useQuery(["chapters"], async () => {
+  //   const res = await axios.get("https://api.quran.com/api/v3/chapters");
+  //   return res.data.chapters as Chapter[];
+  // });
 
   // const {data, isLoading} = useQuery([chapters], () => {
 
@@ -36,11 +43,13 @@ const HomePage = () => {
       </Head>
 
       <Hero />
+
+      <h1 className="text-xl font-semibold">x: { x}</h1>
       <div className="wrapper py-20">
         <h2 className="uppercase font-semibold text-xl">Surah</h2>
 
         <div className="grid grid-cols-1 md:grid-cols-3 gap-5 sm:grid-cols-2">
-          {data?.map((chapter, i) => (
+          {/* {data?.map((chapter, i) => (
             <Surah
               key={i}
               arabic_name={chapter.name_arabic}
@@ -50,11 +59,20 @@ const HomePage = () => {
               revelation_place={chapter.revelation_place}
               serial={chapter.id}
             />
-          ))}
+          ))} */}
         </div>
       </div>
     </div>
   );
 };
+
+// export const getServerSideProps: GetServerSideProps = axios() => {
+  
+//   return {
+//     props:{
+//       x: 10,
+//     }
+//   }
+// }
 
 export default HomePage;
