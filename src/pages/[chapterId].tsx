b05e8ba8aadd4f786e41df22b5bea76c94a416a7
NishatFariza <nishatfarzia78@gmail.com>
2022-11-01 13:37:32 +0600

    wip:details page

---
+++ src/pages/[chapterId].tsx
@@ -0,0 +1,11 @@
+import { useRouter } from 'next/router'
+import React from 'react'
+
+const ChapterDetails = () => {
+    const router = useRouter()
+  return (
+    <div>ChapterDetails</div>
+  )
+}
+
+export default ChapterDetails;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
+import Link from "next/link";
 import { useState } from "react";
 import Hero from "../components/Hero";
 import Surah from "../components/Surah";
@@ -15,13 +16,6 @@ interface props{
 
 
 const HomePage: NextPage<props> = ({chapters}) => {
-  // const [chapters, setChapters] = useState([]);
-
-  // const { data } = useQuery(["chapters"], async () => {
-  //   const res = await axios.get("https://api.quran.com/api/v3/chapters");
-  //   return res.data.chapters as Chapter[];
-  // });
-
 
   return (
     <div>
@@ -31,30 +25,31 @@ const HomePage: NextPage<props> = ({chapters}) => {
 
       <Hero />
 
-   
       <div className="wrapper py-20">
-        <h2 className="uppercase font-semibold text-xl">Surah</h2>
+        <h2 className="uppercase font-semibold text-xl mb-6">Surah</h2>
 
         <div className="grid grid-cols-1 md:grid-cols-3 gap-5 sm:grid-cols-2">
-           {chapters?.map((chapter, i) => (
-            <Surah
-              key={i}
-              arabic_name={chapter.name_arabic}
-              english_name={chapter.name_simple}
-              meaning={chapter.translated_name.name}
-              number_of_verses={chapter.verses_count}
-              revelation_place={chapter.revelation_place}
-              serial={chapter.id}
-            />
-          ))}  
-          
+          {chapters?.map((chapter, i) => (
+            <Link href={`/test`} key={i}>
+              <a>
+                <Surah
+                  arabic_name={chapter.name_arabic}
+                  english_name={chapter.name_simple}
+                  meaning={chapter.translated_name.name}
+                  number_of_verses={chapter.verses_count}
+                  revelation_place={chapter.revelation_place}
+                  serial={chapter.id}
+                />
+              </a>
+            </Link>
+          ))}
         </div>
       </div>
     </div>
   );
 };
 
-
+// This make sure that the data is fetched before the page is rendered
 export const getServerSideProps: GetServerSideProps = async () => {
     const res = await axios.get("https://api.quran.com/api/v3/chapters");
 
